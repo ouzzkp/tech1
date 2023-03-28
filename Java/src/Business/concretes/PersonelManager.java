6994f63a70deb1946f921451e6d6732b71b02372
ynskaydan <ynskaydan@gmail.com>
2023-03-29 00:14:12 +0300

    service

--- Java/src/Business/concretes/PersonelManager.java
+++ Java/src/Business/concretes/PersonelManager.java
@@ -1,6 +1,30 @@
 package Business.concretes;
 
 import Business.abstracts.PersonelService;
+import Core.Utilities.Results.DataResult;
+import Core.Utilities.Results.Result;
+import Entity.Concretes.Personel;
+
+import java.util.List;
 
 public class PersonelManager implements PersonelService {
+    @Override
+    public DataResult<List<Personel>> getAll() {
+        return null;
+    }
+
+    @Override
+    public DataResult<List<Personel>> getAllSorted() {
+        return null;
+    }
+
+    @Override
+    public DataResult<List<Personel>> getAll(int id) {
+        return null;
+    }
+
+    @Override
+    public Result Add(Personel personel) {
+        return null;
+    }
 }
