342c3fe59a3d80204bd29c7a251eba48e14c0e17
ynskaydan <ynskaydan@gmail.com>
2023-04-02 05:56:46 +0300

    DB Connection
    application.properties

--- personel-management-system/src/main/java/tech1/DataAccess/EmployeeDao.java
+++ personel-management-system/src/main/java/tech1/DataAccess/EmployeeDao.java
@@ -1,6 +1,24 @@
 package tech1.DataAccess;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import tech1.Entity.Concretes.Employee;
 
+import java.util.List;
+
 public interface EmployeeDao extends JpaRepository<Employee,Integer>{
+    Employee getByEmployeeId(int id);
+    List<Employee> getAll();
+    Employee getByEmployeeName(String name);
+    //@Query("From Employee where managerId=:managerId ")
+    List<Employee> getByManagerId(int managerId);
+    //@Query("From Employee where region_id=:regionId")
+    List<Employee> getByRegionId(int regionId);
+    List<Employee> getByDepartmentId(int id);
+    List<Employee> getByDepartmentName(String name);
+    List<Employee> getByRegionName(String name);
+
+
+
+
+
 }
--- personel-management-system/src/main/java/tech1/Entity/Dtos/EmployeeWithNames.java
+++ personel-management-system/src/main/java/tech1/Entity/Dtos/EmployeeWithNames.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class EmployeeWithNames {
+public class EmployeeWithNames { //
     private int id;
     private String firstName;
     private String lastName;
