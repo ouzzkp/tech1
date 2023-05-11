33bd381cd7a713fc7309ae5779ac7adcf8c58155
ynskaydan <ynskaydan@gmail.com>
2023-05-11 13:34:29 +0300

    Remove ignored files

--- src/main/java/com/tech1/personelmanagementsystem/Core/Utilities/Logger/abstracts/LoggerService.java
+++
@@ -1,7 +0,0 @@
-package com.tech1.personelmanagementsystem.Core.Utilities.Logger.abstracts;
-
-public interface LoggerService {
-    void logToSystemError(String message);
-    void logToSystemInfo(String message);
-    void logToSystemWarning(String message);
-}
